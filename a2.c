4f56fdd7dce5976445eef390a9c8a1854e8e7b88
zhouchenhua <noatnu@163.com>
2018-12-02 21:32:10 +0800

    5232

--- a2.c
+++ a2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int a2() {
-
+	/*Ëæ±ã´òÓ¡Ò»¾ä»°*/
 	printf("%s \n", "a2()");
 	return 2;
 }
--- a2A.c
+++ a2A.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*获取最后一位数*/
 int a2A() {
 	int a = 12, b = 10;
 	printf("%d \n", a % 10);
